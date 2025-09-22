335391365c8efe02921e0d4fba19437375da6e37
AarthiAyyanar526 <Aarthidev26@gmail.com>
2025-09-22 12:10:11 +0530

    base setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import Register from './Components/Pages/Register'
+import Login from './Components/Pages/Login'
+import Blog from './Components/Pages/Blog'
+import Cart from './Components/Pages/Cart'
+import HomePage from './Components/Pages/HomePage'
+import Payment from './Components/Pages/Payment'
+import ProductDetail from './Components/Pages/ProductDetail'
+import ProductListing from './Components/Pages/ProductListing'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Router>
+          <Routes>
+            <Route path='/' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/blog' element={<Blog/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/home' element={<HomePage/>}/>
+            <Route path='/payment' element={<Payment/>}/>
+            <Route path='/productdetail' element={<ProductDetail/>}/>
+            <Route path='/productlisting' element={<ProductListing/>}/>
+            
+          </Routes>
+          </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
