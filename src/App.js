7d239acc5f7046e78aba0740b6756908f65a2d2d
AarthiAyyanar526 <Aarthidev26@gmail.com>
2025-09-22 12:28:47 +0530

    changes in app file

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             <Route path='/payment' element={<Payment/>}/>
             <Route path='/productdetail' element={<ProductDetail/>}/>
             <Route path='/productlisting' element={<ProductListing/>}/>
-            
+            {/* checking purpose */}
           </Routes>
           </Router>
     </div>
