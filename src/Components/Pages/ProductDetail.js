2aeb8f1f8310e0d2666b1eb6c4f08f070c9e6ca6
Sandy_0626 <santhoshm0626@gmail.com>
2025-10-08 18:07:47 +0530

    product detail update

--- src/Components/Pages/ProductDetail.js
+++ src/Components/Pages/ProductDetail.js
@@ -1,12 +1,29 @@
-import React from 'react'
 
-function ProductDetail() {
+import img1 from "../Assets/image5.jpeg";
+import img2 from "../Assets/image1.jpeg";
+import img3 from "../Assets/image2.jpeg";
+import img4 from "../Assets/image3.jpeg";
+import img5 from "../Assets/image4.jpeg";
+import "./ProductDetails.scss";
+
+const ProductDetails = () => {
   return (
-    <div>
-       ProductDetail
-       <p></p>
-    </div>
-  )
-}
+    <div className="product-images">
+      {/* Main Image (fixed) */}
+      <div className="main-image">
+        <img src={img1} alt="Main product" />
+      </div>
 
-export default ProductDetail
+      {/* Thumbnails (static) */}
+      <div className="thumbnails">
+        <img src={img1} alt="Thumbnail 1" />
+        <img src={img2} alt="Thumbnail 2" />
+        <img src={img3} alt="Thumbnail 3" />
+        <img src={img4} alt="Thumbnail 4" />
+        <img src={img5} alt="Thumbnail 5" />
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductDetail from './Components/Pages/ProductDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductDetail />
   </React.StrictMode>
 );
 
